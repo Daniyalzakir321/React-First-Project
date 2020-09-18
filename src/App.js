f510b8c8ad920d27f0c500aedd8fcf101626d79e
Daniyal Zakir <41327126+Daniyalzakir321@users.noreply.github.com>
2020-09-17 21:24:26 -0700

    React Project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <img src={logo} className="logo-left" />
+      <img src={logo} className="logo-right"/>
+
+      <h1>Hello World From</h1>
+      <h1><a className="App-link" href="https://github.com/Daniyalzakir321" target="_blank"> DANIYAL </a> </h1>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
