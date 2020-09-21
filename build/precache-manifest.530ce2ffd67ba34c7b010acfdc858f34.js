2f3b5f2547752b81ad308c8a3efc80643168659c
Daniyal Zakir <41327126+Daniyalzakir321@users.noreply.github.com>
2020-09-21 15:28:47 +0500

    React First Web App

---
+++ build/precache-manifest.530ce2ffd67ba34c7b010acfdc858f34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64bb32aee74638ce7a0584de719d332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9282c9198fd404a5bb5a",
+    "url": "/static/css/main.78e07154.chunk.css"
+  },
+  {
+    "revision": "fa21119c85047f9caa55",
+    "url": "/static/js/2.333fe94a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.333fe94a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9282c9198fd404a5bb5a",
+    "url": "/static/js/main.ec69724b.chunk.js"
+  },
+  {
+    "revision": "85f1076f9d8b815a5f95",
+    "url": "/static/js/runtime-main.b3eba484.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
